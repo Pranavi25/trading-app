7bf699deeb899c041f10de97ad930c21522f57ed
Pranavi25 <pranavi.punyamanthula.jrvs.ca@gmail.com>
2019-08-12 18:59:01 +0000

    Docker and Jenkins

---
+++ JenkinsFile.sh
@@ -0,0 +1,73 @@
+
+pipeline{
+
+    agent any
+
+    tools {
+
+        maven "Maven"
+
+        jdk "Java"
+
+    }
+
+
+
+    environment {
+
+        app_name = "trading-app"
+
+    }
+
+
+
+    stages {
+
+        stage('Build') {
+
+            steps {
+
+                sh 'mvn clean package'
+
+                echo "app_name is ${env.app_name}"
+
+                archiveArtifacts 'target/*zip'
+
+            }
+
+        }
+
+        stage('Deploy_dev') {
+
+            when { branch 'develop' }
+
+            steps {
+
+                echo "Current branch is ${env.GIT_BRANCH}"
+
+                sh "bash ./scripts/eb_deploy.sh trading-app TradingApp-dev"
+
+            }
+
+        }
+
+        stage('Deploy_prod') {
+
+            when { branch 'master' }
+
+            steps {
+
+                echo "Current branch is ${env.GIT_BRANCH}"
+
+                sh "bash ./scripts/eb_deploy.sh trading-app TradingApp-prod"
+
+            }
+
+        }
+
+    }
+
+}
+
+
+
